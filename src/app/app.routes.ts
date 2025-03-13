1fc6da74abce5199db6de27b76f6d7c58cd787a0
Nagendra Gandla <nagendragandla003@gmail.com>
2025-03-13 12:28:49 -0500

    router changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { ProductsComponent } from './products/products.component';
+import { UsersComponent } from './users/users.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "admin",
+        pathMatch: "full"
+    },
+    {
+        path: "admin",
+        component: AdminComponent
+    },
+    {
+        path: "products",
+        component: ProductsComponent
+    },
+    {
+        path: "users",
+        component: UsersComponent
+    },
+    {
+        path: "**",
+        component: NotFoundComponent
+    }
+];
